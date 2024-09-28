146d866e59941de6acbd7c875cbfa6d9d4c8a823
John Mervin Bocatcat <johnmervin.bocatcat@gmail.com>
2024-09-28 17:21:53 +0800

    Added markdown library

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import "./App.css";
 import useWebSocket, { ReadyState } from "react-use-websocket";
+import ReactMarkdown from "react-markdown";
 interface History {
   type: "prompt" | "result";
   message: string;
@@ -51,24 +52,22 @@ function App() {
     setPrompt(e.target.value);
   }
   return (
-    <>
-      <div>
-        <form onSubmit={onSubmitQuery}>
-          <textarea onChange={onChangePrompt} value={prompt}></textarea>
-          <button type="submit" disabled={readyState !== ReadyState.OPEN || responding}>
-            Send
-          </button>
-        </form>
+    <div className="app">
+      <ul className="result-list">
+        {history.map((h, idx) => (
+          <li key={idx} className={`result-list-item result-list-item--${h.type}`}>
+            <ReactMarkdown className="result-box">{h.message}</ReactMarkdown>
+          </li>
+        ))}
+      </ul>
 
-        <ul>
-          {history.map((h, idx) => (
-            <li key={idx}>
-              <pre className={`result-box result-box--${h.type}`}>{h.message}</pre>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+      <form onSubmit={onSubmitQuery}>
+        <textarea onChange={onChangePrompt} value={prompt} className="prompt-input-box"></textarea>
+        <button className="prompt-input-button" type="submit" disabled={readyState !== ReadyState.OPEN || responding}>
+          Send
+        </button>
+      </form>
+    </div>
   );
 }
 
